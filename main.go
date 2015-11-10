901366e54693b2b17bd76d6011f76bf9fe181b2e
Lily Tsai <tslilyai@gmail.com>
2015-11-10 13:57:02 -0500

    Actually add main file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/tslilyai/SLYcoin/app"
+)
+
+func main() {
+	log.Println("Welcome to SLYcoin!")
+	flag.Parse()
+
+	app, err := app.NewApp(envVars)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = app.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
