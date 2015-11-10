1288c6a025024590623394565b15e51930f4fd23
Lily Tsai <tslilyai@gmail.com>
2015-11-10 13:54:44 -0500

    Add main file for slycoin app

--- test_user_api.cc
+++
@@ -1,8 +0,0 @@
-#include "user_api"
-#include <cstring>
-
-using namespace std
-
-int main() {
-    return RegisterPublicKey("hello world");
-}
--- user_api.hh
+++
@@ -1,7 +0,0 @@
-#include <cstring>
-
-using namespace std;
-
-int RegisterPublicKey(string key);
-int RevokeAndUpdate(string new_key, string signature);
-int GetPublicKey(string email);
