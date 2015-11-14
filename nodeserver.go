73c700222d1788c2ad3ea565bb9a4e51c2b70a4e
Yihe Huang <yihehuang@g.harvard.edu>
2015-11-14 13:48:00 -0500

    sketchy block processing code
    pretty sure it doesn't compile

--- nodeserver.go
+++ nodeserver.go
@@ -87,6 +87,175 @@ func (ns *NodeServer) RemoteBlockLookup(args *RequestBlockArgs, reply *RequestBl
 	return fmt.Errorf(ErrNotFound)
 }
 
+// Yihe's processing thread:
+// checks the queue for incomings and notify worker thread
+// if necessary
+func (ns *NodeServer) ProcessBlock() error {
+	for {
+		ns.mMu.Lock()
+		ns.qMu.Lock()
+		if ns.blkQueue.Count() > 0 {
+			b := ns.blkQueue.Pop()
+			ns.qMu.Unlock()
+			if ns.blockSanityCheck() == false {
+				// skip garbage blocks
+				continue
+			}
+			b1, ok := BlockChain[b.SeqNum]
+			if !ok {
+				// fill up block chain here
+				if ns.processUnseenBlock(b) {
+					// signal worker of block b.SeqNum
+				}
+			} else {
+				if (b1 != b) {
+					// peerCheckAndFixBlock will return the new max
+					// sequence after it fixes the block chian
+					// returns 0 if our block is valid
+					max = ns.peerCheckAndFixBlock(b)
+					// signal worker of new job here
+				}
+			}
+		} else {
+			ns.qMu.Unlock()
+		}
+		ns.mMu.Unlock()
+	}
+}
+
+// making sure the block isn't random garbage...
+func (ns *NodeServer) blockSanityCheck(b Block) bool {
+	// something like b.BlockHash() == b.Hash && b.Hash < target
+}
+
+// Precondition: mMu acquired
+func (ns *NodeServer) processUnseenBlock(b Block) bool {
+	// block b is not seen before, make sure the largest block
+	// we have is correct
+	seq := b.SeqNum - 1
+	for {
+		_, ok := BlockChain[seq]
+		if ok {
+			break
+		} else {
+			seq = seq - 1
+		}
+	}
+
+	// now we have seq as the highest block we have in the BlockChain
+	maxb := BlockChain[seq]
+	// make sure our highest block is still valid
+	// if not, throw away that block and back track
+	for matches, peer_block := peerCheckBlock(maxb), !matches {
+		// add/override user's key
+		// XXX actually this is the place to roll back database
+		// if we ever want to do it
+		ns.recordBlock(peer_block)
+		BlockChain[seq] = peer_block
+		seq--
+		maxb = BlockChain[seq]
+	}
+
+	// now we know everything so far seems valid, check if b can be
+	// based on top of us
+	if ns.validateHash && ns.validateTransaction {
+		ns.recordBlock(b)
+		BlockChain[b.SeqNum] = b
+		return true
+	}
+	return false
+}
+
+// Precondition: mMu acquired
+func (ns *NodeServer) peerCheckAndFixBlock(b Block) uint64 {
+	our_block = BlockChain[b.SeqNum]
+	conflict := false
+	seq := b.SeqNum
+	for matches, peer_block := peerCheckBlock(our_block), !matches {
+		if !validateHash || !validateTransaction {
+			// we are screwed, return current seq
+			return seq
+		}
+		ns.recordBlock(peer_block)
+		BlockChain[seq] = peer_block
+		seq--
+		our_block = BlockChain[seq]
+		conflict = true
+	}
+	if !conflict {
+		return 0
+	}
+	// if conflicts, seq is the forking position
+	seq++
+	for {
+		exists, peer_block := ns.peerRequestBlock(seq)
+		if !exists {
+			break
+		}
+		if ns.validateHash && validateTransaction {
+			// adds block to database + blockchain
+			ns.recordBlock(peer_block)
+			BlockChain[seq] = peer_block
+			seq++
+		} else {
+			// we have a problem here, just return the current seq
+			// number
+			return seq
+		}
+	}
+	return (seq - 1)
+}
+
+func (ns *NodeServer) peerCheckBlock(ob Block) (bool, Block) {
+	exists, peer_block := peerRequestBlock(ob.SeqNum)
+	if !exists || ns.BlockCompare(ob, peer_block) == true {
+		return true, Block{}
+	}
+	return false, peer_block
+}
+
+func (ns *NodeServer) peerRequestBlock(uint64 seq) (bool, Block) {
+	// perform sanity checks on all received blocks
+	// only return blocks that represent received majority
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	blockMap := make(map[[]byte]Block)
+	blockCount := make(map[[]byte]uint64)
+	wg.Add(len(ns.peers))
+	for _, peer := range ns.peers {
+		go func(peer string){
+			found, blk := ns.RequestBlock(peer, seq)
+			if found && ns.blockSanityCheck(blk) {
+				mu.Lock()
+				blockMap[blk.Hash] = blk
+				v, ok := blockCount[blk.Hash]
+				if !ok {
+					v = 1
+				} else {
+					v++
+				}
+				blockMap[blk.Hash] = v
+				mu.Unlock()
+			}
+			wg.Done()
+		}(peer)
+	}
+	wg.Wait()
+
+	var max_count uint64 = 0
+	var max_count_hash []byte = nil
+	for hash, count := range blockMap {
+		if count > max_count {
+			max_count_hash = hash
+			max_count = count
+		}
+	}
+	if max_count_hash == nil {
+		return false, Block{}
+	}
+	return true, blockMap[max_count_hash]
+}
+
 // compute the proof of work and then add the block to our queue
 func (ns *NodeServer) WorkOnBlock(pBlock Block, c chan Block) error {
 	for {
