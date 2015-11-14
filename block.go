6aca5a82139d7cf94d33429b27cff2c7c7c6ffeb
Lily Tsai <tslilyai@gmail.com>
2015-11-14 12:49:06 -0500

    Fix hashing scheme

--- block.go
+++ block.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/rsa"
 	"crypto/sha256"
@@ -9,7 +10,7 @@ import (
 	"fmt"
 )
 
-const numZeros = 28
+const NumZeros = 28
 
 type Block struct {
 	Transactions []Transaction
@@ -36,12 +37,14 @@ var (
 	}
 )
 
+// gets the hash of the block when the proof of work is already computed
 func (b *Block) GetHash(parentHash []byte) []byte {
 	toHash := append(b.strToHash(parentHash), b.ProofOfWork...)
 	checksum := sha256.Sum256(toHash)
 	return checksum[:]
 }
 
+// computes the string of parenthash + transaction json strings
 func (b *Block) strToHash(parentHash []byte) []byte {
 	var (
 		toHash    = parentHash
@@ -50,28 +53,10 @@ func (b *Block) strToHash(parentHash []byte) []byte {
 	)
 
 	for _, txn := range b.Transactions {
-		jsonBytes, err = json.Marshal(&Transaction{
-			Type:      txn.Type,
-			Email:     txn.Email,
-			PublicKey: txn.PublicKey,
-		})
+		jsonBytes, err = json.Marshal(&txn)
 		toHash = append(toHash, jsonBytes...)
 	}
 	return toHash
-}
-
-// add a transaction to a block, beginning work on a block if the node is not currently working on a block
-func addToBlock(t Transaction) {
-	CurrentBlock.Transactions = append(CurrentBlock.Transactions, t)
-}
-
-// clear the current block to fill up another block with transactions
-func clearCurrentBlock() {
-	CurrentBlock = Block{
-		Transactions: nil,
-		SeqNum:       CurrentBlock.SeqNum + 1,
-		ProofOfWork:  []byte{},
-	}
 }
 
 // compute and set the proof of work and hash of the block
@@ -80,7 +65,7 @@ func (b *Block) setProofOfWork(parentHash []byte) {
 	toHash := b.strToHash(parentHash)
 
 	// resulting hash must begin with numZero zeros
-	target := uint64(^uint64(0) >> numZeros)
+	target := uint64(^uint64(0) >> NumZeros)
 	nonceBuf := make([]byte, 8)
 	nonce := uint64(0)
 
@@ -97,27 +82,26 @@ func (b *Block) setProofOfWork(parentHash []byte) {
 	b.Hash = checksum[:]
 }
 
-// verify proof of work
-// check that the block's parent's hash matches the hash of the parent block (seqNum - 1)
-// validate block with respect to the block chain
-func validate(b *Block) error {
+// verify proof of work -- invariant: the parent exists in the map
+// 		- check that the block's parent's hash matches the hash of the parent block (seqNum - 1)
+func (b *Block) validateHash() error {
 	// VALIDATE BLOCK'S HASH (Proof of Work)
-	buf := make([]byte, 16)
-	// buffer is hash of (transactions + parent + pow)
-	binary.PutUvarint(buf, BlockChain[b.SeqNum-1].Hash+b.ProofOfWork)
-	target := uint64(^uint64(0) >> 28)
-	checksum := sha256.Sum256(buf)
-	hash := binary.BigEndian.Uint64(checksum[0:32])
+	toHash := b.GetHash(BlockChain[b.SeqNum-1].Hash)
+	target := uint64(^uint64(0) >> NumZeros)
+	checksum := sha256.Sum256(toHash)
 
 	// ensure checksum begins with 28 0s
-	if hash > target {
+	if binary.BigEndian.Uint64(checksum[0:32]) > target {
 		return fmt.Errorf("invalid proof of work, hash does not begin with 28 0s")
 	}
 	// ensure that block hash matches hash of parent + proof of work
-	if hash != b.Hash {
+	if bytes.Equal(checksum[:], b.Hash) {
 		return fmt.Errorf("hash does not match that of parent")
 	}
+	return nil
+}
 
+func (b *Block) validateTxn() error {
 	// VALIDATE WITH RESPECT TO BLOCK CHAIN by finding the most recent transaction pertaining to the user,
 	// checking the signature with the transaction to be added or verifying the CASig if there has been no
 	// user-made transaction thus far.
@@ -136,6 +120,10 @@ func validate(b *Block) error {
 				break
 			}
 		}
+
+		// get the bytes to hash
+		jsonBytes, err := json.Marshal(&txn)
+
 		// did not find previous transaction of this user
 		// must be a registration and signed by the CA
 		if !found {
@@ -143,13 +131,14 @@ func validate(b *Block) error {
 				return fmt.Errorf("Cannot update a nonexistent public key")
 			}
 			// verify the CA signed this request
-			if err := rsa.VerifyPKCS1v15(&CAKey, crypto.SHA256, []byte(txn.Email), txn.Signature); err != nil {
+
+			if err := rsa.VerifyPKCS1v15(&CAKey, crypto.SHA256, jsonBytes, txn.Signature); err != nil {
 				return fmt.Errorf("Not signed by the CA")
 			}
 			return nil
 		}
 		// else this is an update
-		if err := rsa.VerifyPKCS1v15(&lastTxn.PublicKey, 0, []byte{}, lastTxn.Signature); err != nil {
+		if err := rsa.VerifyPKCS1v15(&lastTxn.PublicKey, crypto.SHA256, jsonBytes, lastTxn.Signature); err != nil {
 			if txn.Type != Update {
 				return fmt.Errorf("Cannot register if you already are in the database")
 			}
@@ -157,4 +146,18 @@ func validate(b *Block) error {
 		}
 	}
 	return nil
+}
+
+// add a transaction to a block, beginning work on a block if the node is not currently working on a block
+func addToBlock(t Transaction) {
+	CurrentBlock.Transactions = append(CurrentBlock.Transactions, t)
+}
+
+// clear the current block to fill up another block with transactions
+func clearCurrentBlock() {
+	CurrentBlock = Block{
+		Transactions: nil,
+		SeqNum:       CurrentBlock.SeqNum + 1,
+		ProofOfWork:  []byte{},
+	}
 }
