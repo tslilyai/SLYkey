63471d34fb540cf87c484176b7c6f1a85fb70624
Lily Tsai <tslilyai@gmail.com>
2015-11-14 13:08:09 -0500

    Fix a few glitches in validate

--- verifier.go
+++ verifier.go
@@ -1,11 +1,11 @@
 package main
 
 // TODO(Serena): put these functions in block.go
-func validateTransactionsAndUpdateDatabase (b *Block) error {
+func validateTransactionsAndUpdateDatabase(b *Block) error {
 	// TODO(Serena):
 	// validate block transactions and update database simultaneously.
-	// Iterate from the beginning of the block. 
-	// Assumes that the database is correct and up to date for the // all blocks prior to the current block. 
+	// Iterate from the beginning of the block.
+	// Assumes that the database is correct and up to date for the // all blocks prior to the current block.
 	// Thus, uses the database values as the most recent user key.
 	// Also validates CA signature for creation of a new key.
 }
@@ -18,18 +18,19 @@ func validateBlockAndUpdateDatabase(b *Block) error {
 
 func verifyBlockChainAndUpdateDatabase() {
 	// for each block in block chain
-	seqNum = uint64(0)
+	seqNum := uint64(0)
 	// Sync: need to lock whole blockchain here by creating
 	// getBlock function instead.
-	while block, ok := BlockChain[args.SeqNum]; ok {
-		// If that returned an error, raise error here. 
-		// Proper go error handling?
-		validateBlockAndUpdateDatabase(block)
-		seqNum++
+	for i := 0; i < len(BlockChain); i++ {
+		if block, ok := BlockChain[args.SeqNum]; ok {
+			// If that returned an error, raise error here.
+			// Proper go error handling?
+			validateBlockAndUpdateDatabase(block)
+			seqNum++
+		}
 	}
 }
 
-func runVerifier () {
+func runVerifier() {
 	verifyBlockChainAndUpdateDatabase()
 }
-
